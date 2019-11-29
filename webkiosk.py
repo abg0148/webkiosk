62a344af82388a832e8a297e2f3b6d0dd5ea9f96
Abhinav Gupta <abhinavgupta0148@gmail.com>
2019-11-29 22:20:36 +0530

    working for srs automatic fill

--- webkiosk.py
+++ webkiosk.py
@@ -10,7 +10,7 @@ user = ["*********"]    #username
 pwd = ["**********"]    #password
 
 #driver=webdriver.Chrome()
-driver=webdriver.Chrome(options=chrome_options)
+driver=webdriver.Chrome("C:/Users/Abhinav Gupta/Downloads/chromedriver.exe",options=chrome_options)
 
 driver.get("https://webkioskintra.thapar.edu:8443/")
 
@@ -20,9 +20,26 @@ elem = driver.find_element_by_name("Password")
 elem.send_keys(pwd[0])
 elem.send_keys(Keys.RETURN)
 
-time.sleep(5)
-window_after = driver.window_handles[0]
+time.sleep(10)
+#window_after = driver.window_handles[0]
 #then execute the switch to window method to move to newly opened window
 
-##driver.switch_to_window(window_after)
-##print(driver.current_url)
+
+
+#driver.switch_to_window(window_after)
+while(1):
+    print(driver.get("https://webkioskintra.thapar.edu:8443/StudentFiles/SRS/StudNewSrsEntry.jsp"))
+
+    time.sleep(10)
+    driver.find_element_by_xpath("/html/body/table[2]/tbody/tr[3]/td/input").click()
+
+    time.sleep(10)
+    if(driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[7]/td/input").get_attribute("value")=="Send Feedback"):
+        driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[7]/td/input").click()
+    else:
+        driver.find_element_by_xpath("/html/body/table[3]/tbody/tr[20]/td/input").click()
+
+
+while(1):
+    print('reached here')
+    continue
